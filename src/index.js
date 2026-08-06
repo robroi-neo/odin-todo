a5ee9f0f53ea1b55a36b7d3516ae907189d82045
robroi-neo <robroineo2@gmail.com>
2026-08-06 17:06:45 +0800

    chore: add index.html

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-import projectFactory from "./factories/projectFactory.js";
-import taskFactory from "./factories/taskFactory.js";
 import userFactory from "./factories/userFactory.js";
+import App from "./views/App.js";
 
 const user = userFactory.create("robroi");
-const project = projectFactory.create("Project 1")
-user.addProject(project);
 
-project.addTask(taskFactory.create("task 1"));
-project.addTask(taskFactory.create("task 2"));
-project.addTask(taskFactory.create("task 3"));
-
-const project2 = projectFactory.create("Project 2")
-
-user.addProject(project2);
-project2.addTask(taskFactory.create("task 1"));
-project2.addTask(taskFactory.create("task 2"));
-project2.addTask(taskFactory.create("task 3"));
-
-user.printUserTasks();
-
-// remove task from project2
-
-user.removeProjects(project2.id);
-user.printUserTasks();
-
+const app = new App(user);
+app.mount(document.getElementById("app"));
